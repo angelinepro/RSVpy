aeab08d29675559660f38cc919a683f525000ff7
Joshua Kwan <joshk@triplehelix.org>
2015-02-23 23:13:17 -0500

    The rest of the website

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,9 @@ from .views import TemplateSettingsView
 urlpatterns = patterns('',
     url(r'^$', TemplateSettingsView.as_view(template_name='index.html')),
     url(r'^contact/?$', TemplateSettingsView.as_view(template_name='contact.html')),
+    url(r'^travel/?$', TemplateSettingsView.as_view(template_name='travel.html')),
+    url(r'^accommodation/?$', TemplateSettingsView.as_view(template_name='accommodation.html')),
+    url(r'^registry/?$', TemplateSettingsView.as_view(template_name='registry.html')),
     url(r'^r/', include('invites.urls', namespace='invites')),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
